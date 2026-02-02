56b0789ea72170f8752778c6d30cdefc5d681db4
shreyas hathiwala <116088100+shreyas3155@users.noreply.github.com>
2026-02-02 14:56:02 +0530

    Update homepage image source from 'homepage.jpg' to 'HomeHero.jpg'

--- reactfolio-master/src/pages/homepage.jsx
+++ reactfolio-master/src/pages/homepage.jsx
@@ -104,7 +104,7 @@ const Homepage = () => {
 								<div className="homepage-image-container">
 									<div className="homepage-image-wrapper">
 										<img
-											src="homepage.jpg"
+											src="HomeHero.jpg"
 											alt="about"
 											className="homepage-image"
 										/>
